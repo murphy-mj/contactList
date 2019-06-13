a2cbec995c8dd3f1b2a4303cb3be4b8401160af6
murphy mj <2martinmurphy@gmail.com>
2019-06-13 21:22:49 +0100

    contact component completed

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,15 @@ import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Header from '../src/components/header/'
 import FilterControls from '../src/components/filterControls/'
+import Contact from '../src/components/contact/'
+
+const sample = {
+    name: {first:'Joe', last:'Bloggs'},
+    email: 'j.bloggs@example.com',
+    phone: '012-3456789',
+    picture: {thumbnail: './profile.png'}
+}
+
 
 storiesOf("Contact List App/Header", module).add("default", () => (
     <Header noContacts={10} />
@@ -12,4 +21,9 @@ storiesOf("Contact List App/Header", module).add("default", () => (
 // NEW STORY
 storiesOf("Contact List App/Filter Controls", module).add("default", () => (
     <FilterControls />
+))
+
+//  . . . . other stories . . . .
+storiesOf("Contact List App/Contact", module).add("default", () => (
+    <Contact contact={sample}/>
 ));
