274286840e91fab1ca3601113f67639a9da85dcc
murphy mj <2martinmurphy@gmail.com>
2019-06-13 21:07:52 +0100

    FilterControls component completed

--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Header from '../src/components/header/'
+import FilterControls from '../src/components/filterControls/'
 
 storiesOf("Contact List App/Header", module).add("default", () => (
     <Header noContacts={10} />
+));
+
+
+// NEW STORY
+storiesOf("Contact List App/Filter Controls", module).add("default", () => (
+    <FilterControls />
 ));
