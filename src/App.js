702e97a005489ef20a7c10a11aa9ce51e92865a5
murphy mj <2martinmurphy@gmail.com>
2019-06-16 16:05:51 +0100

    Added StubAPI

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import React, { Component } from "react";
 import Header from "./components/header/";
 import ContactList from "./components/contactList/";
 import FilterControls from "./components/filterControls/";
+import request from "superagent";
+import api from "./datastore/stubAPI";
+
+
 
 class App extends Component {
-  render() {
-    const sample = {
-      name: { first: "Joe", last: "Bloggs" },
-      email: "j.bloggs@example.com",
-      phone: "012-3456789",
-      picture: { thumbnail: "./profile.png" }
-    };
 
-    const contacts = [sample, sample, sample, sample, sample];
+      //const sample = {
+      //  name: { first: "Joe", last: "Bloggs" },
+      //  email: "j.bloggs@example.com",
+      //  phone: "012-3456789",
+      //  picture: { thumbnail: "./profile.png" }
+      //};
 
+
+ componentDidMount(){
+      request.get("https://randomuser.me/api/?results=10").end((error, res) => {
+          if (res) {
+              let {results: contacts} = JSON.parse(res.text);
+              //let contacts = JSON.parse(res.text);
+              api.initialize(contacts);
+              this.setState({});
+          } else {
+              console.log(error);
+          }
+      });
+  }
+
+
+
+   // const contacts = [sample, sample, sample, sample, sample];
+render() {
+    let contacts = api.getAll();
     return (
         <div className="jumbotron">
-          <Header noContacts={5} />
+          <Header noContacts={contacts.length} />
           <FilterControls />
           <ContactList contacts={contacts} />
         </div>
