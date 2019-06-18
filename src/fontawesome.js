169c5f107dda609536ba69faffb7d1f8610fa7d6
murphy mj <2martinmurphy@gmail.com>
2019-06-18 21:27:12 +0100

    Routing set up complete

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -1,7 +1,15 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+    faEnvelope,
+    faPhone,
+    faHome,
+    faBirthdayCake,
+    faArrowCircleLeft
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(
     faEnvelope,
     faPhone
 );
+
+library.add(faEnvelope, faPhone, faHome, faBirthdayCake, faArrowCircleLeft);
