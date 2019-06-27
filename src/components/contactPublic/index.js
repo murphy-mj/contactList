990d9d04b0443f1a6408b85356bf8da39aff2097
murphy mj <2martinmurphy@gmail.com>
2019-06-27 22:14:55 +0100

    Added Google map support

--- src/components/contactPublic/index.js
+++ src/components/contactPublic/index.js
@@ -5,9 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./contactPublic.css";
 import { Link } from "react-router-dom";
 import PublicProfile from "../contactPublic/publicProfile"
+import Map from "../map";
+const REACT_APP_GAPI_KEY1 = process.env.REACT_APP_GAPI_KEY;
+
 
 export default ({ user }) => {
+
     const name = capitalize(`${user.name.first} ${user.name.last}`);
+    const location ={lng:-7.142379, lat: 52.246502} // WIT
     return (
         <Fragment>
             <div className="row">
@@ -28,9 +33,19 @@ export default ({ user }) => {
                 <div className="col-4">
                     <PublicProfile user={user} />
                 </div>
-                <div className="col-5 bg-secondary text-light">
-                    <span>Map placeholder</span>
+                <div className="col-5" >
+                    <Map
+                        isMarkerShown
+                        googleMapURL = "https://maps.googleapis.com/maps/api/js?key=#{REACT_APP_GAPI_KEY1}&v=3.exp&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        location={location}
+                        containerElement={<div style={{ height: `400px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
                 </div>
+
+
+
             </div>
         </Fragment>
     );
